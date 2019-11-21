7fbe7a1633d7b164de72fc9af2b2cf7037b4e415
JustYourAverageIdiot <151667@csgaugustinus.nl>
2019-11-21 09:09:01 +0000

    yeet

--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -1,12 +1,14 @@
 function setup() {
   canvas = createCanvas(450,450);
-  background('orange');
+  background('darkred');
   canvas.parent('processing');
   noLoop();
 }
 
 function draw() {
   noStroke();
-  fill('darkred');
-  triangle(225,0,0,450,450,450);
+  fill('pink')
+  triangle(450,0,0,225,450,450)
+  fill('purple');
+  triangle(0,0,0,450,450,225);
 }
